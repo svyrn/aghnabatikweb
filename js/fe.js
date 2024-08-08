193d5949dc988d4546481448318c969f9834a916
svyrn <yaosao86@gmail.com>
2024-08-09 00:28:27 +0700

    add frontend home

--- js/fe.js
+++ js/fe.js
@@ -1,6 +1,13 @@
 // toggle
 const navbarNav = document.querySelector(".navbar-nav");
-
-document.querySelector("#menus").onclick = () => {
-  navbarNav.classList.toggle("act");
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
+
+/* klik luar sidebar */
+const menuNav = document.querySelector("#menu");
+document.addEventListener("click", function (e) {
+  if (!menuNav.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
